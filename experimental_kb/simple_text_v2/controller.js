dea05fb4fd6b0731d36f63a0ce0c839d98e24f78
XhitHub <com.trojan.exe@gmail.com>
2021-07-13 12:38:29 +0800

    simple text v2

---
+++ experimental_kb/simple_text_v2/controller.js
@@ -0,0 +1,27 @@
+const SIDES = ['lhs', 'rhs'];
+
+class Controller {
+  constructor(funcsMap) {
+    this.funcsMap = funcsMap
+  }
+
+  addSearchForm(gRule) {
+    var searchForm = {}
+    SIDES.forEach(xhs => {
+      resXhs = []
+      gRule[xhs].map(gFact => {
+  
+      })
+    })
+  }
+}
+// add search form for the gRule
+const addSearchForm = (gRule) => {
+  var searchForm = {}
+  SIDES.forEach(xhs => {
+    resXhs = []
+    gRule[xhs].map(gFact => {
+
+    })
+  })
+}
--- experimental_kb/simple_text_v2/sample_data.js
+++ experimental_kb/simple_text_v2/sample_data.js
@@ -4,24 +4,14 @@ const facts = [
 ]
 
 // gRules with relationship functions
-const gRulesV1 = [
-  {
-    lhs: [
-      '$1 is in $2 at time $3',
-      '$2 has termination at time $3'
-    ],
-    // consequence can be N new facts
-    rhs: [
-      '$1 is terminated at time $sum($3,1)',
-    ]
-  }
-]
 
-const gRulesV2 = [
+const gRulesV3 = [
   {
     lhs: [
       '${char} is in ${room} at time ${time}',
-      '${room} has termination at time ${sum(time, -1)}',
+      '${room} has termination at time ${{func: "sum", args: [{arg:"time"}, "arg val X of type being string", 1]}}',
+      // nested funcs
+      '${room} has termination at time ${{func: "sum", args: [{func: "product", args: [{arg: "time"}, 2]}]}}',
     ],
     // consequence can be N new facts
     rhs: [
@@ -29,3 +19,29 @@ const gRulesV2 = [
     ]
   }
 ]
+/*
+JSON parse the func obj
+  then get reverse version of func with the func key, from reversed funcs map
+cannot distinguish arg and directValArg of string type
+  valArgs: []? cannot as will lose order info
+  indicator: args: [{arg:"time"}, "arg val X of type being string", 1]
+    in processing: foreach arg: if (arg.arg) {is arg, need to lookup arg map} else {is direct val}
+*/
+
+// relationshipFuncMap
+const funcsMap = {
+  sum: (a, b) => {
+    return a+b
+  },
+  product: (a, b) => {
+    return a*b
+  },
+}
+
+const reverseFuncsMap = {
+  sum: (res, argsMap) => {
+    if (argsMap.a == null) {
+      
+    }
+  }
+}
